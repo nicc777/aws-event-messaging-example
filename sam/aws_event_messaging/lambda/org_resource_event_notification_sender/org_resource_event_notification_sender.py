8476543f2b7e456390609aa28b0eb6774326364b
Nico Coetzee <nicc777@gmail.com>
2022-07-30 13:36:11 +0200

    feat: wip on templates

--- sam/aws_event_messaging/lambda/org_resource_event_notification_sender/org_resource_event_notification_sender.py
+++ sam/aws_event_messaging/lambda/org_resource_event_notification_sender/org_resource_event_notification_sender.py
@@ -11,7 +11,28 @@ else:
     logger.setLevel(logging.INFO)
 
 
+HUMAN_READABLE_TEMPLATE = 'Infrastructure Source: __SOURCE__\nResource Identifier: __ARN__\nEvent: __EVENT__'
 
+
+def extract_message_from_record(record: dict)->dict:
+    logger.info('record={}'.format(json.dumps(record, default=str)))
+    if 'body' in record:
+        body = json.loads(record['body'])
+        logger.info('body={}'.format(json.dumps(body, default=str)))
+        if 'Message' in body:
+            message = json.loads(body['Message'])
+            logger.info('message={}'.format(json.dumps(message, default=str)))
+            return message
+        else:
+            raise Exception('Expected "Message" key')
+    else:
+        raise Exception('Expected "body" key')
+
+
+def prepare_human_readable_message(origin_message: str)->str:
+    hm = 'Not yet Implemented...'
+    logger.info('origin_message={}'.format(origin_message))
+    return hm
 
 
 def handler(event, context):
@@ -23,68 +44,47 @@ def handler(event, context):
     .. code-block:: json
 
         {
-            "Records": [
-                {
-                    "messageId": "oooooooooo",
-                    "receiptHandle": "oooooooooo",
-                    "body": "{\n  \"Type\" : \"Notification\",\n  \"MessageId\" : \"oooooooooo\",\n  \"TopicArn\" : \"arn:aws:sns:eu-central-1:000000000000:aws-event-messaging-stack-AwsServiceEventsTopic-oooooooooo\",\n  \"Message\" : \"{\\\"version\\\":\\\"0\\\",\\\"id\\\":\\\"oooooooooo\\\",\\\"detail-type\\\":\\\"EC2 Instance State-change Notification\\\",\\\"source\\\":\\\"aws.ec2\\\",\\\"account\\\":\\\"000000000000\\\",\\\"time\\\":\\\"2022-07-30T08:40:32Z\\\",\\\"region\\\":\\\"eu-central-1\\\",\\\"resources\\\":[\\\"arn:aws:ec2:eu-central-1:000000000000:instance/i-0b4c854ee43718713\\\"],\\\"detail\\\":{\\\"instance-id\\\":\\\"i-0b4c854ee43718713\\\",\\\"state\\\":\\\"running\\\"}}\",\n  \"Timestamp\" : \"2022-07-30T08:40:32.470Z\",\n  \"SignatureVersion\" : \"1\",\n  \"Signature\" : \"oooooooooo\",\n  \"SigningCertURL\" : \"oooooooooo\",\n  \"UnsubscribeURL\" : \"oooooooooo\"\n}",
-                    "attributes": {
-                        "ApproximateReceiveCount": "1",
-                        "SentTimestamp": "1659170432500",
-                        "SenderId": "oooooooooo",
-                        "ApproximateFirstReceiveTimestamp": "1659170432504"
-                    },
-                    "messageAttributes": {},
-                    "md5OfBody": "oooooooooo",
-                    "eventSource": "aws:sqs",
-                    "eventSourceARN": "arn:aws:sqs:eu-central-1:000000000000:aws-event-messaging-stack-AwsServiceEventsQueue-oooooooooo",
-                    "awsRegion": "eu-central-1"
-                }
-            ]
-        }
-
-    The body record includes a Message attribute with it's contents in JSON below:
-
-    .. highlight:: json
-    .. code-block:: json
-
+    "Records": [
         {
-            "version": "0",
-            "id": "oooooooooo",
-            "detail-type": "EC2 Instance State-change Notification",
-            "source": "aws.ec2",
-            "account": "000000000000",
-            "time": "2022-07-30T08: 40: 32Z",
-            "region": "eu-central-1",
-            "resources": [
-                "arn:aws:ec2:eu-central-1:000000000000:instance/i-oooooooooo"
-            ],
-            "detail": {
-                "instance-id": "i-oooooooooo",
-                "state": "running"
-            }
+            "messageId": "oooooooooo",
+            "receiptHandle": "oooooooooo",
+            "body": "{\n  \"Type\" : \"Notification\",\n  \"MessageId\" : \"oooooooooo\",\n  \"TopicArn\" : \"arn:aws:sns:eu-central-1:0000000000:aws-event-messaging-stack-OrgResourceEventsTopic-oooooooooo\",\n  \"Subject\" : \"AWS Event\",\n  \"Message\" : \"aws.ec2,arn:aws:ec2:eu-central-1:0000000000:instance/i-0b4c854ee43718713,{\\\"instance-id\\\": \\\"i-0b4c854ee43718713\\\", \\\"state\\\": \\\"running\\\"}\",\n  \"Timestamp\" : \"2022-07-30T09:48:08.637Z\",\n  \"SignatureVersion\" : \"1\",\n  \"Signature\" : \"oooooooooo\",\n  \"SigningCertURL\" : \"oooooooooo\",\n  \"UnsubscribeURL\" : \"oooooooooo\"\n}",
+            "attributes": {
+                "ApproximateReceiveCount": "1",
+                "SentTimestamp": "1659174488670",
+                "SenderId": "oooooooooo",
+                "ApproximateFirstReceiveTimestamp": "1659180249279"
+            },
+            "messageAttributes": {},
+            "md5OfBody": "oooooooooo",
+            "eventSource": "aws:sqs",
+            "eventSourceARN": "arn:aws:sqs:eu-central-1:0000000000:aws-event-messaging-stack-OrgResourceEventsMessagingQueue-oooooooooo",
+            "awsRegion": "eu-central-1"
         }
+    ]
+}
+
     """
 
     logger.info('event={}'.format(json.dumps(event, default=str)))
-    # client = boto3.client('sns')
-    # target_sns_topic = os.getenv('TOPIC_ARN', None)
-    # if target_sns_topic is None:
-    #     raise Exception('Environment variable TOPIC_ARN must be set')
+    client = boto3.client('sns')
+    target_sns_topic = os.getenv('TOPIC_ARN', None)
+    if target_sns_topic is None:
+        raise Exception('Environment variable TOPIC_ARN must be set')
 
-    # if 'Records' in event:
-    #     for record in event['Records']:
-    #         message = extract_message_from_record(record=record)
-    #         for event_msg in build_standard_event_message(origin_message=message):
-    #             logger.info('Sending message to SNS Topic OrgResourceEventsTopic: {}'.format(event_msg))
-    #             response = client.publish(
-    #                 TopicArn=target_sns_topic,
-    #                 Message=event_msg,
-    #                 Subject='AWS Event'
-    #             )
-    #             logger.info('response={}'.format(response))
-    # else:
-    #     raise Exception('Event does not contain key "Records"')
+    if 'Records' in event:
+        for record in event['Records']:
+            message = extract_message_from_record(record=record)
+            human_readable_message = prepare_human_readable_message(origin_message=message)
+            logger.info('Sending message to SNS Topic OrgResourceEventsNotificationTopic: {}'.format(human_readable_message))
+            response = client.publish(
+                TopicArn=target_sns_topic,
+                Message=human_readable_message,
+                Subject='AWS Resource State Change Event'
+            )
+            logger.info('response={}'.format(response))
+    else:
+        raise Exception('Event does not contain key "Records"')
 
     return {
         "statusCode": 200,
