1689fa6261342490378aa0337aba19cbf726d91c
Nico Coetzee <nicc777@gmail.com>
2022-07-30 11:41:51 +0200

    feat: wip on templates

--- sam/aws_event_messaging/lambda/aws_service_event_handler/aws_service_event_handler.py
+++ sam/aws_event_messaging/lambda/aws_service_event_handler/aws_service_event_handler.py
@@ -2,6 +2,7 @@ import json
 import boto3
 import os
 import logging
+import copy
 
 logger = logging.getLogger()
 if os.getenv('DEBUG', None) is not None:
@@ -26,6 +27,18 @@ def extract_message_from_record(record: dict)->dict:
             raise Exception('Expected "Message" key')
     else:
         raise Exception('Expected "body" key')
+
+
+def build_standard_event_message(origin_message: dict)->list:
+    messages = list()
+    for resource in origin_message['resources']:
+        message = copy.deepcopy(MESSAGE_TEMPLATE)
+        message = message.replace('__EVENT_SOURCE__', origin_message['source'])
+        message = message.replace('__RESOURCE_ID__', resource)
+        message = message.replace('__EVENT__', json.dumps(origin_message['detail']))
+        logger.info('Generated message: {}'.format(message))
+        messages.append(message)
+    return messages
 
 
 def handler(event, context):
@@ -81,6 +94,7 @@ def handler(event, context):
     """
 
     logger.info('event={}'.format(json.dumps(event, default=str)))
+    client = boto3.client('sns')
     target_sns_topic = os.getenv('TOPIC_ARN', None)
     if target_sns_topic is None:
         raise Exception('Environment variable TOPIC_ARN must be set')
@@ -88,6 +102,14 @@ def handler(event, context):
     if 'Records' in event:
         for record in event['Records']:
             message = extract_message_from_record(record=record)
+            for event_msg in build_standard_event_message(origin_message=message):
+                logger.info('Sending message to SNS Topic OrgResourceEventsTopic: {}'.format(event_msg))
+                response = client.publish(
+                    TopicArn=target_sns_topic,
+                    Message=event_msg,
+                    Subject='AWS Event'
+                )
+                logger.info('response={}'.format(response))
     else:
         raise Exception('Event does not contain key "Records"')
 
